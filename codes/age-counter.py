b94f31ecf3a92e42c929a5b46165db8bfc5112d7
ᴀᴅɪᴛʏᴀ ᴊʜᴀ <119198739+PyAaditya@users.noreply.github.com>
2022-11-28 21:10:36 +0530

    Create age-counter.py

---
+++ codes/age-counter.py
@@ -0,0 +1,9 @@
+# TG :- @PythonXgamer
+# Support :- @ViolenceChitChat
+
+age = int(input("What's Your Current Age?\n- "))
+years = 95 - age
+days = years * 365
+weeks = years * 52
+months = years * 12
+print(f"You have {days} days, {weeks} weeks and {months} months left.")
